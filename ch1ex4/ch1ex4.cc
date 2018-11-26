5cf09c45293ba4b7ec5a1a8a4bd341d1f5f6069b
Charles Shapiro <charles.shapiro@tomshiro.org>
2018-11-26 17:10:55 -0500

    Exercise 4.  Note amusing hack to make sure program exits when window is closed.

--- ch1ex4/ch1ex4.cc
+++ ch1ex4/ch1ex4.cc
@@ -16,68 +16,74 @@
  */
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 using namespace cv;
 
-/*
-Calculate frame rate of camera from actual capture.  CAP_PROP_FPS is often double the actual frame rate.
-*/
-float calculateFrameRate(VideoCapture& theCapture)
-{
-   const int num_frames=120;
-   Mat timerFrame;
-   double startTix = getTickCount();
-   double endTix;
-   
-   for (int kk=0; kk < num_frames; ++kk)
-      theCapture >> timerFrame;
-   endTix=getTickCount();
-   double tixTaken=endTix - startTix;
-   double calculatedFPS=num_frames / (tixTaken / getTickFrequency());
-   cout << "Calculated FPS:" << calculatedFPS << endl;
-   return calculatedFPS;
+const int PYRDOWNMAX=8;
+int CurrentPyrdownNum=0;
 
+int SliderPos=0;
+
+
+/*
+ Callback when trackbar is moved.
+ */
+void onTrackBarSlide(int pos, void *)
+{
+   CurrentPyrdownNum=pos;
 }
 
 int main(int argc, char *argv[]) {
-   if(argc < 2)
+   if(argc > 1)
    {
-      cout << "Oops. Please provide output filename" << endl;
+      cout << "Oops. This program takes no arguments." << endl;
       exit(1);
    }
+
+
+  
    namedWindow("output",cv::WINDOW_AUTOSIZE);
+   createTrackbar("Downsample","output",&SliderPos,PYRDOWNMAX,onTrackBarSlide);
    
    VideoCapture capture(0);
 
-   // We are pyrdowning the image, so it is half the size of the camera frame size.
-   // Therefore, divide the frame in half.  Without this, the avi will not play properly
-   // in players such as vlc or dragon.
-   cv::Size size( (int)capture.get(CV_CAP_PROP_FRAME_WIDTH)/2,
-		  (int)capture.get(CV_CAP_PROP_FRAME_HEIGHT)/2
-		  );
-   
-   VideoWriter writer;
-   
-   writer.open(argv[1],CV_FOURCC('M','J','P','G'),calculateFrameRate(capture),size);
-   
    Mat bgr_frame;
    Mat pyrd_frame;
    char c;
+   
    while(true) {
       capture >> bgr_frame;
 
       if(bgr_frame.empty())
 	 break;
-      pyrDown(bgr_frame,pyrd_frame);
+      if(CurrentPyrdownNum)
+	 pyrDown(bgr_frame,pyrd_frame);
+      else
+	 pyrd_frame=bgr_frame;
+      for(int kk=0;kk<(CurrentPyrdownNum-1);++kk)
+	 pyrDown(pyrd_frame,pyrd_frame);
+      
+      // This checks if the window itself has been closed.  If so, I exit the loop.
+      // Without this check, imshow() recreates the window without the slider bar and
+      // the only way out is to ctrl-c the program. It doesn't matter which property you
+      // get, except that WND_PROP_VISIBLE seems to be -1 if the window is on-screen.
+      if(-1 == getWindowProperty("output",WND_PROP_AUTOSIZE))
+	 break;
+
       imshow("output",pyrd_frame);
-      writer << pyrd_frame;
+      
+      
       c = (char)waitKey(33);
       if(27 == c)
 	 break;
+      
    }
+   
    capture.release();
    
    
