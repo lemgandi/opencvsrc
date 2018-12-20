dc3adabe7c085f66b71d6cc41d4ac61a61325d74
Charles Shapiro <charles.shapiro@tomshiro.org>
2018-12-20 09:20:02 -0600

    Chapter 3 exercise 1

--- template.cc
+++ template.cc
@@ -1,17 +1,17 @@
 /*
  Exercise 3 from Intro to OpenCV: write downsampled avi from camera to disk
 
-    This file is part of opencvsr.
-    opencvsr is free software: you can redistribute it and/or modify
+    This file is part of opencvsrc.
+    opencvsrc is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    opencvsr is distributed in the hope that it will be useful,
+    opencvsrc is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
-    along with opencvsr.  If not, see <http://www.gnu.org/licenses/>.
+    along with opencvsrc.  If not, see <http://www.gnu.org/licenses/>.
 
  */
 
@@ -23,3 +23,8 @@
 
 using namespace std;
 using namespace cv;
+
+
+int main(int argc, char *argv[])
+{
+}
