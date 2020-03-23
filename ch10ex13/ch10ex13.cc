58c92c8995813577fbe56de7baf0384fec5f449a
Charles Shapiro <charles.shapiro@tomshiro.org>
2020-03-23 18:41:41 -0400

    ch10ex13. What is param1?

--- ch10ex13/ch10ex13.cc
+++ ch10ex13/ch10ex13.cc
@@ -39,19 +39,57 @@ Usage Message
  */
 void usage(const char *pname)
 {
-   cout << "Usage: " << pname << " -i infile" << endl;
+   cout << "Usage: " << pname << " -i infile [-m maximum] [-t threshold] [-a]" << endl;
+   cout << "Show image threshold transformations. maximum is the highest value, threshold is thre threshold value." << endl;
+   cout << "The a switch converts to adaptive threshold; for this, the threshold argument becores the weighting constant" << endl;
+   cout << "It seems to have good results at if maximum =~ 25" << endl;
+   
 }
 
+/*
+Shpw image with threshold type and threshold given.
+ */
+imageWindow * makeImageWindow(const Mat & image,int threshtype,int maxval,int threshval,const char *wname,bool adaptive)
+{
+   Mat myImage;
+   
+   if(adaptive)
+      adaptiveThreshold(image,myImage,(double)maxval,ADAPTIVE_THRESH_MEAN_C,threshtype,5,(double)threshval);
+   else
+      threshold(image,myImage,threshval,maxval,threshtype);
+   
+   imageWindow * retVal = new imageWindow(wname,myImage);
+   return retVal;
+      
+}
+/*
+Main Line
+ */
 int main(int argc, char *argv[])
 {
-   const char *optString="i:";
+   int threshold = 128;
+   int maximum = 128;
+   bool adaptive = false;
+   const char *optString="i:t:m:a";
    char opt;
    char *imageFN = 0;
-
+   istringstream digitsConverter;
+   
    while((opt = getopt(argc,argv,optString)) != -1) {
       switch(opt) {
+      case 'a':
+	 adaptive=true;
+	 break;
       case 'i':
 	 imageFN = strdup(optarg);
+	 break;
+      case 'm':
+	 digitsConverter.str(optarg);
+	 digitsConverter >> maximum;
+	 break;
+      case 't':
+	 digitsConverter.str(optarg);
+	 digitsConverter >> threshold;
 	 break;
       default:
 	 usage(argv[0]);
@@ -62,7 +100,7 @@ int main(int argc, char *argv[])
    if(0 == imageFN)
       imageFN=strdup(defaultImageFN);
    
-   Mat theImage=imread(imageFN);
+   Mat theImage=imread(imageFN,IMREAD_GRAYSCALE);
    if (theImage.empty())
    {
       cout << "Oops. Cannot read [" << imageFN << "]" << endl;
@@ -73,10 +111,34 @@ int main(int argc, char *argv[])
       cout << "Read image of type " << type2str(theImage.type()) << endl;
    
    imageWindow *oWindow = new imageWindow("original",theImage);
+   imageWindow *binWindow = makeImageWindow(theImage,THRESH_BINARY,threshold,maximum,"Binary",adaptive);
+   imageWindow *binvWindow = makeImageWindow(theImage,THRESH_BINARY_INV,threshold,maximum,"Binary Inverse",adaptive);
+   
+   imageWindow * truncWindow = 0;
+   imageWindow * tozeroWindow = 0;
+   imageWindow * tozeroInvWindow = 0;
+   
+   if(! adaptive)
+   {
+      truncWindow = makeImageWindow(theImage,THRESH_TRUNC,threshold,maximum,"Trunc",adaptive);
+      tozeroWindow = makeImageWindow(theImage,THRESH_TOZERO,threshold,maximum,"Trunc to zero",adaptive);
+      tozeroInvWindow = makeImageWindow(theImage,THRESH_TOZERO_INV,threshold,maximum,"trunc to zero inverse",adaptive);
+   }
+				     
    char c;
+   
    c = (char)waitKey();
 
    delete oWindow;
+   delete binWindow;
+   delete binvWindow;
+   
+   if(! adaptive)
+   {      
+   delete truncWindow;
+   delete tozeroWindow;
+   delete tozeroInvWindow;
+   }
    free(imageFN);
    
 					 
